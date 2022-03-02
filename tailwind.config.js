b2845fe52c66789b174fe93d693f968696efbbec
Bernard Yang <ybernard@gmail.com>
2022-03-02 03:50:44 -0800

    testing updated font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        body: ["Sofia Pro"],
-        sans: ["Sofia Pro", ...defaultTheme.fontFamily.sans],
-        serif: [...defaultTheme.fontFamily.serif],
-        mono: [...defaultTheme.fontFamily.mono],
+        sans: ["Sofia Pro", "sans-serif"],
       },
       colors: {
         primary: "#F1F1FD",
